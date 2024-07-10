2bd5dd6ad95290c1519ea5b8b1a2591cb189413c
Shathi paul <shathipaul11@gmail.com>
2024-07-10 21:32:41 +0600

    added footer

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="text-center text-sm py-10 px-2">
+      © 2035 by Jessica Priston. All rights are reserved
+    </div>
+  );
 };
 
 export default Footer;
